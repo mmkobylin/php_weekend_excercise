4aa2239be1538c7ce34cb6846acbaad5da04b646
mmkobylin <mmkobylin@gmail.com>
2020-08-08 10:24:59 +0100

    13-largest.php working using foreach

---
+++ 02-arrays-functions/13-largest.php
@@ -0,0 +1,27 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+// … your function here
+
+function largest($array){
+
+    $bigNum = $array[0];
+
+    foreach($array as $number){
+
+        if ($number > $bigNum) {
+            $bigNum = $number;
+        }        
+
+    }
+
+    return $bigNum;
+}
+
+
+dump(
+    largest([2, 4, 6, 4, 7, 5]), // 7
+    largest([-2, 4, 6, 4, 2, -7, 5]), // 6
+    largest([-2, -4, -4, -7, -5]), // -2
+);
