07a141f399c5b286ce1beec0deb0b17c94781135
mmkobylin <mmkobylin@gmail.com>
2020-08-08 09:20:49 +0100

    Php files added to repository

---
+++ 01-basic-php/4chal-loop13.php
@@ -0,0 +1,7 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+for ($i = 0; $i < 1000; $i += 13) {
+    dump($i);
+};
---
+++ 01-basic-php/6chal-cumulate.php
@@ -0,0 +1,8 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+for ($i = 1; $i <= 50; $i += 1) {
+    $sum = 0;
+    dump($sun += $i);
+};
